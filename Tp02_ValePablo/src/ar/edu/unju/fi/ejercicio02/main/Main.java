82428ac30787b672910fc758f15cb159daad54f6
PabloVale2024 <166053449+PabloVale2024@users.noreply.github.com>
2024-04-25 15:48:14 -0300

    final ejercico2

--- Tp02_ValePablo/src/ar/edu/unju/fi/ejercicio02/main/Main.java
+++ Tp02_ValePablo/src/ar/edu/unju/fi/ejercicio02/main/Main.java
@@ -1,10 +1,21 @@
 package ar.edu.unju.fi.ejercicio02.main;
 
+import ar.edu.unju.fi.ejercicio02.constantes.Mes;
+import ar.edu.unju.fi.ejercicio02.model.Efemeride;
+import ar.edu.unju.fi.ejercicio2.util.Listado;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
+		Efemeride efe1= new Efemeride();
+		
+		efe1.setCodigo("01");
+		efe1.setMes(Mes.ABRIL);
+		efe1.setDia(19);
+		efe1.setDetalle("fundacionde Jujuy");
+		Listado.listadoEfemerides.add(efe1);
 
-	}
-
+}
 }
